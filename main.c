7dc54cfe9381368b6dd95db19dbf0cc04681f53d
KERROUCHI Narimane <narimaneker19@gmail.com>
2024-11-15 18:26:58 +0100

    maj abr

--- main.c
+++ main.c
@@ -1,52 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <listbd.h>
-
+#include "abr.h"
 // void bst_operations();   // Function to manage BST operations
-void btree_operations(); // Function to manage B-tree operations
+// void btree_operations(); // Function to manage B-tree operations
 
 int main()
 {
     int choice;
-
-    while (1)
+    Noeud *racine = NULL;
+    do
     {
         printf("\nSelect a Data Structure:\n");
-        printf("1. Binary Search Tree (BST)\n");
-        printf("2. B-Tree\n");
-        printf("3. Exit\n");
+        printf("1. Listes doublement chaînées: \n");
+        printf("2. Arbre binaire de recherche (BST) : \n");
+        printf("3. B-Tree\n");
+        printf("4. Tas (Heap)\n");
+        printf("5. Quitter\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice)
         {
-case 1:
-{
-printf("Listes doublement chaînées: \n\n");
-//fonction()
-break;
-}
-case 2:
-{
-printf(" Arbre binaire de recherche (BST) : \n\n");
-//fonction()
-break;
-}
-case 3:
-{
-printf("B-tree : \n\n");
-//fonction()
-break;
-}
-case 4:
+        case 1:
+        {
+            printf("Listes doublement chaînées: \n");
+            // fonction()
+            break;
+        }
+        case 2:
+        {
+            printf(" Arbre binaire de recherche (BST) : \n\n");
+            menu_abr(&racine);
+            break;
+        }
+        case 3:
+        {
+            printf("B-tree : \n\n");
+            // fonction()
+            break;
+        }
+        case 4:
 
-{
-printf("Tas (Heap) : \n\n");
-//fonction()
-break;
-}
-}
-    }
+        {
+            printf("Tas (Heap) : \n\n");
+            // fonction()
+            break;
+        }
+        case 5:
+            printf("Quitter le programme.\n");
+            break;
+        }
+    } while(choice!=5);
 
     return 0;
 }
